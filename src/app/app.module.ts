3e462e99ae20499eacbd2f931d7fa9790d816ede
Ender Lobo <jlobo24081988@gmail.com>
2017-11-23 14:59:07 -0400

    AdminLTE template schema - partial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,20 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+import { HomeComponent } from './front/home/home.component';
+
+import { DashboardComponent } from './admin/dashboard/dashboard.component';
+import { AdminHeaderComponent } from './admin/includes/admin-header/admin-header.component';
+import { AdminFooterComponent } from './admin/includes/admin-footer/admin-footer.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    DashboardComponent,
+    AdminHeaderComponent,
+    AdminFooterComponent,
   ],
   imports: [
     BrowserModule
